1a39b56864ca2a907aca5cfee159bedd0a675547
azagatti <andre.zagatti1@gmail.com>
2020-08-04 10:18:00 -0300

    feat: create db connection

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -4,6 +4,11 @@ import cors from 'cors';
 const app = express();
 
 app.use(cors());
+app.use(express.json());
+
+app.get('/users', (req, res) => {
+  return res.json({});
+});
 
 app.listen(3333, () => {
   console.log('Server running');
